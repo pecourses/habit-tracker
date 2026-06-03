62086186b15482d4c1dd363d5bb68cbd2d706f25
Viktoriia <pecourses>
2026-06-03 18:46:26 +0300

    Update yup schema

--- client/src/utils/validationSchemas.ts
+++ client/src/utils/validationSchemas.ts
@@ -1,15 +1,22 @@
 import * as yup from 'yup'
 import { HABIT_FREQUENCIES } from '../constants'
 
-export const HABIT_VALIDATION_SCHEMA = yup.object({
-  title: yup
-    .string()
-    .min(1, 'Title is too short')
-    .max(50, 'Title is too long')
-    .required('Input title'),
-  frequency: yup
-    .string()
-    .oneOf(HABIT_FREQUENCIES, 'Select frequency')
-    .required('Select frequency'),
-  isActive: yup.boolean().required()
-})
+import type { ObjectSchema } from 'yup'
+import type { CreateHabitDto, HabitFrequency } from '../types'
+
+export const HABIT_VALIDATION_SCHEMA: ObjectSchema<CreateHabitDto> = yup.object(
+  {
+    title: yup
+      .string()
+      .min(1, 'Title is too short')
+      .max(50, 'Title is too long')
+      .required('Input title'),
+    frequency: yup
+      .mixed<HabitFrequency>()
+      .oneOf(HABIT_FREQUENCIES, 'Select frequency')
+      .required('Select frequency'),
+    isActive: yup.boolean().required()
+  }
+)
+
+// interface Person extends yup.InferType<typeof personSchema> {}
